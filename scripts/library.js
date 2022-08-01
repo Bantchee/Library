fa7d9e8ca630774edde7004d8ed2ce04e48f32c4
Jar'Zeno <jarzenojarrett@gmail.com>
2022-08-01 14:35:09 -0700

    Update script

--- scripts/library.js
+++ scripts/library.js
@@ -1,11 +1,20 @@
 let myLibrary = [];
 
+addBookToLibrary(new Book("The Name of the Wind", "Patrick Rothfuss", 662, "#", true));
+addBookToLibrary(new Book("The Institue", "Stephen King", 560, "#", false));
+addBookToLibrary(new Book("The Rage of Dragons", "Evan Winter", 438, "#", true));
+addBookToLibrary(new Book("The Game of Thrones", "George R.R. Martin", 819, "#", false));
+
+const article = document.querySelector('article');
+
 // Book Object Constructor
-function Book(title, author, pages, read) {
+function Book(title, author, pages, url, read) {
     this.title = title;     // String
     this.author = author;   // String
-    this.pages = pages;      // Number
+    this.pages = pages;     // Number
+    this.url = url;         // String
     this.read = read;       // Boolean
+
     this.info = () => {
         return `${title} by ${author}, ${pages} pages, ${(read) ? "have read": "not read yet"}`;
     }
@@ -16,7 +25,14 @@ function addBookToLibrary(book) {
 }
 
 function render() {
-    for(let i = 0; i < myLibrary.length; i++) {
+    for(let book in myLibrary) {
+        console.log(myLibrary[book])
+
+        const div = document.createElement('div');
         
+        div.textContent = myLibrary[book].author;
+        article.appendChild(div);   
     }
 }
+
+render();
