540f823e5dad5f97752c05d0336b10e1833584cc
Jar'Zeno <jarzenojarrett@gmail.com>
2022-08-02 18:31:44 -0700

    Work on render function

--- scripts/library.js
+++ scripts/library.js
@@ -1,19 +1,19 @@
 let myLibrary = [];
 
-addBookToLibrary(new Book("The Name of the Wind", "Patrick Rothfuss", 662, "#", true));
-addBookToLibrary(new Book("The Institue", "Stephen King", 560, "#", false));
-addBookToLibrary(new Book("The Rage of Dragons", "Evan Winter", 438, "#", true));
-addBookToLibrary(new Book("The Game of Thrones", "George R.R. Martin", 819, "#", false));
+addBookToLibrary(new Book("The Name of the Wind", "Patrick Rothfuss", 662, true, "./images/cover-the-name-of-the-wind.jpg"));
+addBookToLibrary(new Book("The Institue", "Stephen King", 560, false, "#"));
+addBookToLibrary(new Book("The Rage of Dragons", "Evan Winter", 438, true, "#"));
+addBookToLibrary(new Book("The Game of Thrones", "George R.R. Martin", 819, false, "#"));
 
 const article = document.querySelector('article');
 
 // Book Object Constructor
-function Book(title, author, pages, url, read) {
+function Book(title, author, pages, read, url) {
     this.title = title;     // String
     this.author = author;   // String
     this.pages = pages;     // Number
-    this.url = url;         // String
     this.read = read;       // Boolean
+    this.url = url;         // String
 
     this.info = () => {
         return `${title} by ${author}, ${pages} pages, ${(read) ? "have read": "not read yet"}`;
@@ -28,10 +28,32 @@ function render() {
     for(let book in myLibrary) {
         console.log(myLibrary[book])
 
-        const div = document.createElement('div');
-        
-        div.textContent = myLibrary[book].author;
-        article.appendChild(div);   
+        // Create Book Card
+        const divBook = document.createElement('div');
+        divBook.classList.add('book');
+
+        // Create Title
+        const title = document.createElement('p');
+        title.classList.add('title');
+        title.textContent = myLibrary[book].title;
+
+        // Create Cover
+        const cover = document.createElement('img');
+        cover.classList.add('cover');
+        cover.setAttribute("src", myLibrary[book].url);
+
+        // Create Author
+        const author = document.createElement('p');
+        author.classList.add('author');
+        author.textContent = myLibrary[book].author;
+
+        // Add Title, Cover, Author to Book
+        divBook.appendChild(title);
+        divBook.appendChild(cover);
+        divBook.appendChild(author);
+
+        // Add Book to Article
+        article.appendChild(divBook);   
     }
 }
 
