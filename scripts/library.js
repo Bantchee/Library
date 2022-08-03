c298458743c236d07b57c1ce4c9d90c9bb6d1ed5
Jar'Zeno <jarzenojarrett@gmail.com>
2022-08-03 11:47:23 -0700

    Update UI

--- scripts/library.js
+++ scripts/library.js
@@ -1,9 +1,9 @@
 let myLibrary = [];
 
 addBookToLibrary(new Book("The Name of the Wind", "Patrick Rothfuss", 662, true, "./images/cover-the-name-of-the-wind.jpg"));
-addBookToLibrary(new Book("The Institue", "Stephen King", 560, false, "#"));
-addBookToLibrary(new Book("The Rage of Dragons", "Evan Winter", 438, true, "#"));
-addBookToLibrary(new Book("The Game of Thrones", "George R.R. Martin", 819, false, "#"));
+addBookToLibrary(new Book("The Institue", "Stephen King", 560, false, "./images/cover-the-institue.jpg"));
+addBookToLibrary(new Book("The Rage of Dragons", "Evan Winter", 438, true, "./images/cover-the-rage-of-dragons.jpg"));
+addBookToLibrary(new Book("The Game of Thrones", "George R.R. Martin", 819, false, "./images/cover-the-game-of-thrones.jpg"));
 
 const article = document.querySelector('article');
 
@@ -26,8 +26,6 @@ function addBookToLibrary(book) {
 
 function render() {
     for(let book in myLibrary) {
-        console.log(myLibrary[book])
-
         // Create Book Card
         const divBook = document.createElement('div');
         divBook.classList.add('book');
@@ -47,10 +45,56 @@ function render() {
         author.classList.add('author');
         author.textContent = myLibrary[book].author;
 
+        // Create Div.Icon-Container
+        const divIcons = document.createElement('d');
+        divIcons.classList.add('icon-container');
+
+            // Create Page Div
+            const divPage = document.createElement('div');
+            divPage.classList.add('div-page', 'div-icon');
+
+                // Create Page Count PopUpText
+                const popUpText = document.createElement('span');
+                popUpText.classList.add('pop-up-text');
+                popUpText.textContent = myLibrary[book].pages;
+
+                // Create Page Count Icon
+                const pageIcon = document.createElement('img');
+                pageIcon.classList.add('page-icon', 'icons');
+                pageIcon.setAttribute('src', 'icons/pages.svg');
+                pageIcon.addEventListener('click', () => {
+                    popUpText.classList.toggle('show');
+                });
+
+            divPage.appendChild(pageIcon);
+            divPage.appendChild(popUpText);
+
+            // Create Read Icon
+            const readIcon = document.createElement('img');
+            readIcon.classList.add('read-icon', 'icons');
+            readIcon.setAttribute('src', 'icons/read.svg');
+
+            // Create Edit Icon Button
+            const editIcon = document.createElement('img');
+            editIcon.classList.add('edit-icon', 'icons');
+            editIcon.setAttribute('src', 'icons/edit.svg');
+
+            // Create Delete Icon Button
+            const deleteIcon = document.createElement('img');
+            deleteIcon.classList.add('delete-icon', 'icons');
+            deleteIcon.setAttribute('src', 'icons/delete.svg');
+
+        // Add Icons to Icon Container
+        divIcons.appendChild(divPage);
+        divIcons.appendChild(readIcon);
+        divIcons.appendChild(editIcon);
+        divIcons.appendChild(deleteIcon);
+
         // Add Title, Cover, Author to Book
         divBook.appendChild(title);
         divBook.appendChild(cover);
         divBook.appendChild(author);
+        divBook.appendChild(divIcons);
 
         // Add Book to Article
         article.appendChild(divBook);   
@@ -58,3 +102,7 @@ function render() {
 }
 
 render();
+
+function myFunction() {
+    console.log("Hello World!")
+}
